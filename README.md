9eb7a4506f8909d9e98fc15bc83f7eb4a6be4ae9
jbpashu <jbpashu@gmail.com>
2018-05-08 17:10:34 +0530

    API updated and readme updated with more info

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 Symfony Demo Application
 ========================
 
-The "Symfony Demo Application" is a reference application created to show how
-to develop Symfony applications following the recommended best practices.
-
-[![Build Status](https://travis-ci.org/symfony/symfony-demo.svg?branch=master)](https://travis-ci.org/symfony/symfony-demo)
+The "Blog Demo Application" is a blog application.
 
 Requirements
 ------------
@@ -20,30 +17,15 @@ information.
 Installation
 ------------
 
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-First, install the [Symfony Installer](https://github.com/symfony/symfony-installer)
-if you haven't already. Then, install the Symfony Demo Application executing
-this command anywhere in your system:
+Clone this application on you local using following command:
 
 ```bash
-$ symfony demo
+$ mkdir blog_demo
+$ cd blog_demo
+$ git clone https://github.com/jbpashu/my-blog.git
+$ composer install --no-interaction
 
-# if you're using Windows:
-$ php symfony demo
 ```
-
-If the `demo` command is not available, update your Symfony Installer to the
-most recent version executing the `symfony self-update` command.
-
-> **NOTE**
->
-> If you can't use the Symfony Installer, download and install the demo
-> application using Git and Composer:
->
->     $ git clone https://github.com/symfony/symfony-demo symfony_demo
->     $ cd symfony_demo/
->     $ composer install --no-interaction
 
 Usage
 -----
@@ -52,7 +34,7 @@ There is no need to configure a virtual host in your web server to access the ap
 Just use the built-in web server:
 
 ```bash
-$ cd symfony_demo/
+$ cd blog_demo/
 $ php bin/console server:run
 ```
 
@@ -68,16 +50,26 @@ terminal.
 > For more details, see:
 > https://symfony.com/doc/current/cookbook/configuration/web_server_configuration.html
 
-Troubleshooting
+Accessing API
 ---------------
+There are two API endpoints have been created to check API documents please check <http://127.0.0.1:8000/api/doc> or <http://localhost:8000>.
+1. GET /{_locale}/api/blogs/list.{_format} -  This will give you a list of all the blog created.
+2. GET /{_locale}/api/blogs/posts/{id} - This will give you a detailed view of the blog you just need to pass the blog id.
 
-The current Symfony Demo application uses Symfony 3.x version. If you want to
-use the legacy Symfony 2.8 version, clone the Git repository and checkout the
-`v0.8.4` tag, which is the last one compatible with Symfony 2.8:
+Unit Testing
+---------------
+You can perform unit testing also on this project using following commands:
 
+Linux or Mac OS
+---------------
 ```bash
-$ git clone https://github.com/symfony/symfony-demo symfony_demo
-$ cd symfony_demo/
-$ git checkout tags/v0.8.4 -b v0.8.4
-$ composer install
+$ cd blog_demo/
+$ ./vendor/bin/simple-phpunit
+```
+
+Windows
+---------------
+```bash
+$ cd blog_demo/
+$ vendor\bin\simple-phpunit
 ```
--- src/AppBundle/Controller/BlogAPIController.php
+++ src/AppBundle/Controller/BlogAPIController.php
@@ -1,204 +1,95 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Comment;
 use AppBundle\Entity\Post;
-use AppBundle\Events;
-use AppBundle\Form\CommentType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\GenericEvent;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
-//use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Util\Codes;
-use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\RouteRedirectView;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * Controller used to manage blog contents in the public part of the site.
+ * Controller to manage blog API contents.
  *
  * @Route("api/blogs")
- *
- * @author Ryan Weaver <weaverryan@gmail.com>
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
 class BlogAPIController extends FOSRestController {
 
-    /**
-     * get blog list
-     *
-     * This function returns list of blogs
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   statusCodes = {
-     *     200 = "Returned when successful",
-     *   }
-     * )
-     *
-     * @Annotations\View()
-     *
-     * @return array
-     * @Route("/list.{_format}")
-     * @return array returns array containing agent details
-     *
-     */
-    public function getBlogListAction() {
-        $em = $this->getDoctrine()->getManager();
-        $posts = $em->getRepository(Post::class)->findAll();
-        
-        $view = View::create();
-            
-        if($posts) {
-            $view->setStatusCode(200)->setData($posts);  
-        } else {
-            $view->setStatusCode(404)->setData(['errorMessage'=>'No Posts found']);  
-        }
-        
-        return $this->get('fos_rest.view_handler')->handle($view); 
-    }
+	/**
+	 * get blog list
+	 *
+	 * This function returns list of blog
+	 *
+	 * @ApiDoc(
+	 *   resource = true,
+	 *   statusCodes = {
+	 *     200 = "Returned when successful",
+	 *     404 = "When No Blog found"
+	 *   }
+	 * )
+	 *
+	 * @Annotations\View()
+	 *
+	 * @Route("/list.{_format}")
+	 * @Method("GET")
+	 * @return array returns array containing blog details
+	 *
+	 */
+	public function getBlogListAction( Request $request, $_format ) {
+		$em    = $this->getDoctrine()->getManager();
+		$posts = $em->getRepository( Post::class )->findLatestBlogPosts();
 
-    /**
-     * @Route("/posts/{slug}", name="blog_post")
-     * @Method("GET")
-     *
-     * NOTE: The $post controller argument is automatically injected by Symfony
-     * after performing a database query looking for a Post with the 'slug'
-     * value given in the route.
-     * See https://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/converters.html
-     */
-    public function postShowAction(Post $post) {
-        // Symfony provides a function called 'dump()' which is an improved version
-        // of the 'var_dump()' function. It's useful to quickly debug the contents
-        // of any variable, but it's not available in the 'prod' environment to
-        // prevent any leak of sensitive information.
-        // This function can be used both in PHP files and Twig templates. The only
-        // requirement is to have enabled the DebugBundle.
-        if ('dev' === $this->getParameter('kernel.environment')) {
-            dump($post, $this->getUser(), new \DateTime());
-        }
-        $post->setViews($post->getViews() + 1);
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
-        $em->flush();
+		$view = View::create();
 
-        return $this->render('blog/post_show.html.twig', ['post' => $post]);
-    }
+		if( $posts ) {
+			$view->setStatusCode( 200 )->setData( $posts )->setFormat( $_format );
+		} else {
+			$view->setStatusCode( 404 )->setData( [ 'errorMessage' => 'No Posts found' ] );
+		}
 
-    /**
-     * @Route("/comment/{postSlug}/new", name="comment_new")
-     * @Method("POST")
-     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
-     * @ParamConverter("post", options={"mapping": {"postSlug": "slug"}})
-     *
-     * NOTE: The ParamConverter mapping is required because the route parameter
-     * (postSlug) doesn't match any of the Doctrine entity properties (slug).
-     * See https://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/converters.html#doctrine-converter
-     */
-    public function commentNewAction(Request $request, Post $post, EventDispatcherInterface $eventDispatcher) {
-        $comment = new Comment();
-        $comment->setAuthor($this->getUser());
-        $post->addComment($comment);
+		return $this->get( 'fos_rest.view_handler' )->handle( $view );
+	}
 
-        $form = $this->createForm(CommentType::class, $comment);
-        $form->handleRequest($request);
+	/**
+	 * Get single blog post detaild by id
+	 *
+	 * @Route("/posts/{id}")
+	 * @Method("GET")
+	 *
+	 *
+	 * @ApiDoc(
+	 *   resource = true,
+	 *   statusCodes = {
+	 *     200 = "Returned when successful",
+	 *     404 = "When No Blog details found"
+	 *   }
+	 * )
+	 *
+	 * @Annotations\View()
+	 *
+	 * @return array returns array containing blog details
+	 *
+	 */
+	public function getPostAction( Post $post ) {
+		$view = View::create();
+		if( $post->getIsActive() ) {
+			$post->setViews( $post->getViews() + 1 );
+			$em = $this->getDoctrine()->getManager();
+			$em->persist( $post );
+			$em->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($comment);
-            $em->flush();
+			$view->setStatusCode( 200 )->setData( $post )->setFormat( 'json' );
+		} else {
+			$view->setStatusCode( 404 )->setData( [ 'errorMessage' => 'Posts does not exist' ] );
+		}
 
-            // When triggering an event, you can optionally pass some information.
-            // For simple applications, use the GenericEvent object provided by Symfony
-            // to pass some PHP variables. For more complex applications, define your
-            // own event object classes.
-            // See https://symfony.com/doc/current/components/event_dispatcher/generic_event.html
-            $event = new GenericEvent($comment);
-
-            // When an event is dispatched, Symfony notifies it to all the listeners
-            // and subscribers registered to it. Listeners can modify the information
-            // passed in the event and they can even modify the execution flow, so
-            // there's no guarantee that the rest of this controller will be executed.
-            // See https://symfony.com/doc/current/components/event_dispatcher.html
-            $eventDispatcher->dispatch(Events::COMMENT_CREATED, $event);
-
-            return $this->redirectToRoute('blog_post', ['slug' => $post->getSlug()]);
-        }
-
-        return $this->render('blog/comment_form_error.html.twig', [
-                    'post' => $post,
-                    'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * This controller is called directly via the render() function in the
-     * blog/post_show.html.twig template. That's why it's not needed to define
-     * a route name for it.
-     *
-     * The "id" of the Post is passed in and then turned into a Post object
-     * automatically by the ParamConverter.
-     *
-     * @param Post $post
-     *
-     * @return Response
-     */
-    public function commentFormAction(Post $post) {
-        $form = $this->createForm(CommentType::class);
-
-        return $this->render('blog/_comment_form.html.twig', [
-                    'post' => $post,
-                    'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/search", name="blog_search")
-     * @Method("GET")
-     *
-     * @return Response|JsonResponse
-     */
-    public function searchAction(Request $request) {
-        if (!$request->isXmlHttpRequest()) {
-            return $this->render('blog/search.html.twig');
-        }
-
-        $query = $request->query->get('q', '');
-        $posts = $this->getDoctrine()->getRepository(Post::class)->findBySearchQuery($query);
-
-        $results = [];
-        foreach ($posts as $post) {
-            $results[] = [
-                'title' => htmlspecialchars($post->getTitle()),
-                'summary' => htmlspecialchars($post->getSummary()),
-                'url' => $this->generateUrl('blog_post', ['slug' => $post->getSlug()]),
-            ];
-        }
-
-        return $this->json($results);
-    }
+		return $this->get( 'fos_rest.view_handler' )->handle( $view );
+	}
 
 }
